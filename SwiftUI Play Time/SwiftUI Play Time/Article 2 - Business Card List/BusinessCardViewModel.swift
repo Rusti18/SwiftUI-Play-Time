c5ffa4a1ec1f297717bb24ff287cc46a73bc5542
Mihai Rustiuc <mihai.rustiuc@protonmail.com>
2022-10-11 08:49:51 +0300

    Create `BusinessCardViewModel`

---
+++ SwiftUI Play Time/SwiftUI Play Time/Article 2 - Business Card List/BusinessCardViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  BusinessCardViewModel.swift
+//  SwiftUI Play Time
+//
+//  Created by Mihai Andrei Rustiuc on 11.10.2022.
+//
+
+import Foundation
+
+struct BusinessCardViewModel {
+    let imageName: String
+    let name: String
+    let occupation: String
+    let workplace: String
+}
