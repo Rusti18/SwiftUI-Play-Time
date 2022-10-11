a3d601fad6f3fa2ae23999b3a6cc52245166d89c
Mihai Rustiuc <mihai.rustiuc@protonmail.com>
2022-10-11 08:23:06 +0300

    Building the `BuildingCardList` using the `ScrollView` component

---
+++ SwiftUI Play Time/SwiftUI Play Time/Article 2 - Business Card List/BusinessCardList_Scroll.swift
@@ -0,0 +1,35 @@
+//
+//  BusinessCardList_Scroll.swift
+//  SwiftUI Play Time
+//
+//  Created by Mihai Andrei Rustiuc on 11.10.2022.
+//
+
+import SwiftUI
+
+struct BusinessCardList_Scroll: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                BusinessCard(
+                    imageName: "man",
+                    name: "Jack Doe",
+                    occupation: "Plumber",
+                    workplace: "Plumbers LLC"
+                )
+                BusinessCard(
+                    imageName: "man",
+                    name: "John Doe",
+                    occupation: "Dentist",
+                    workplace: "Dentists LLC"
+                )
+            }
+        }
+    }
+}
+
+struct BusinessCardList_Scroll_Previews: PreviewProvider {
+    static var previews: some View {
+        BusinessCardList_Scroll()
+    }
+}
