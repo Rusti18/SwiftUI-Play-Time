09ebc89851055db4a53e58cddf2b7685aacee387
Mihai Rustiuc <mihai.rustiuc@protonmail.com>
2022-10-11 08:22:16 +0300

    Building `BusinessCardList` using the `List` component

---
+++ SwiftUI Play Time/SwiftUI Play Time/Article 2 - Business Card List/BusinessCardList.swift
@@ -0,0 +1,33 @@
+//
+//  BusinessCardList.swift
+//  SwiftUI Play Time
+//
+//  Created by Mihai Andrei Rustiuc on 10.10.2022.
+//
+
+import SwiftUI
+
+struct BusinessCardList: View {
+    var body: some View {
+        List {
+            BusinessCard(
+                imageName: "man",
+                name: "Jack Doe",
+                occupation: "Plumber",
+                workplace: "Plumbers LLC"
+            )
+            BusinessCard(
+                imageName: "man",
+                name: "John Doe",
+                occupation: "Dentist",
+                workplace: "Dentists LLC"
+            )
+        }
+    }
+}
+
+struct BusinessCardList_Previews: PreviewProvider {
+    static var previews: some View {
+        BusinessCardList()
+    }
+}
